b23eeade6cb00ffd0a17d43aa2e5045c0ef756b5
wjy <jianyuan.wei@hand-china.com>
2019-01-15 10:47:07 +0800

    添加部分逻辑代码

--- src/main/java/com/simplify/setup/support/LinkResolve.java
+++ src/main/java/com/simplify/setup/support/LinkResolve.java
@@ -1,9 +1,15 @@
 package com.simplify.setup.support;
 
+import com.simplify.setup.annotation.Link;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
 
 /**
  * 注解@Link解析器
@@ -12,6 +18,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class LinkResolve implements ApplicationContextAware {
+
+    private static final Logger log = LoggerFactory.getLogger(LinkResolve.class);
 
     private ApplicationContext applicationContext;
 
@@ -23,7 +31,40 @@ public class LinkResolve implements ApplicationContextAware {
     }
 
     //todo 用于处理解析 参数待定
-    public void parse(){
+    public void setFieldValueForCollection(Collection cols){
+
+        if(cols == null || cols.isEmpty()) {
+            return;
+        }
+        //获取集合中存储对象的Class类型
+        Class<?> cls = cols.iterator().next().getClass();
+
+        //获取所有的存储对象类的字段
+        Field[] fields = cls.getDeclaredFields();
+
+        for(Field field : fields) {
+            Link link = field.getAnnotation(Link.class);
+            if(link != null) {
+                field.setAccessible(true);
+                //获取目标bean的Class类型
+                Class<?> beanClass = link.targetBean();
+                //取出bean的实例对象
+                Object bean = applicationContext.getBean(beanClass);
+                if(bean == null) {
+                    log.warn("{}没有对应的实例对象",beanClass.getName());
+                    return;
+                }
+                String[] param = link.methodParam();
+                if(param.length != 0) {
+                    //todo 待处理 参数字段非空
+                }
+            }
+        }
+
 
     }
+
+
+
+
 }
